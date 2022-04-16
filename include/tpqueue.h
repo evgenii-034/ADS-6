92adffc967af6a922d8d13a52eaef101632145c2
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-04-16 09:48:49 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,41 +6,40 @@
 template<typename T, int size>
 class TPQueue {
  private:
-    T arr[100];
-    int first, last;
-    
-  public:
-    TPQueue() : first(0), last(0) {}
-     void push(T x) {
-      if (last - first >= size) {
-      throw std::string("Full!");
-        } else {
-            int a;
-            a = last++;
-            while ((--a >= first) && (arr[a % size].prior < x.prior)) {
-                arr[(a+1) % size] = arr[a % size];
-            }
-            arr[(a+1) % size] = x;
-        }
-    }
-    T pop() {
-        if (first == last)
-            throw std::string("Empty!");
-        else
-            return arr[(first++) % size];
-    }
-    T front() {
-        if (first == last)
-            throw std::string("Empty!");
-        else
-            return arr[first % size];
-    }
-    T back() {
-        if (first == last)
-            throw std::string("Empty!");
-        else
-            return arr[(last-1) % size];
-    }
+T arr[100];
+int first, last;
+ public:
+TPQueue() : first(0), last(0) {}
+void push(T x) {
+if (last - first >= size) {
+throw std::string("Full!");
+} else {
+int a;
+a = last++;
+while ((--a >= first) && (arr[a % size].prior < x.prior)) {
+arr[(a+1) % size] = arr[a % size];
+}
+arr[(a+1) % size] = x;
+}
+}
+T pop() {
+if (first == last)
+throw std::string("Empty!");
+else
+return arr[(first++) % size];
+}
+T front() {
+if (first == last)
+throw std::string("Empty!");
+else
+return arr[first % size];
+}
+T back() {
+if (first == last)
+throw std::string("Empty!");
+else
+return arr[(last-1) % size];
+}
 };
 
 struct SYM {
